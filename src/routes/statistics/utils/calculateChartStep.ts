b3bf14260ed4973379cec57954eb5089bd0970f8
Mariia <mariia.kondak@nure.ua>
2024-11-01 17:48:08 +0100

    Updated calculation steps function

--- src/routes/statistics/utils/calculateChartStep.ts
+++ src/routes/statistics/utils/calculateChartStep.ts
@@ -1,25 +1,38 @@
-export default (realMax: number, maxStepsAmount: number = 7): { maxChartPoint: number; step: number } => {
-    let currentPower = -1;
-    let rightStep;
-    while (!rightStep) {
-        let beautifulNumbers = [
-            10 * 10 ** currentPower,
-            20 * 10 ** currentPower,
-            25 * 10 ** currentPower,
-            50 * 10 ** currentPower
-        ];
-        if (currentPower === -1) {
-            beautifulNumbers = [1, 2, 5];
-        }
-        for (const currentStep of beautifulNumbers) {
-            const outputsRightAmountOfSteps = Math.ceil(realMax / currentStep) <= maxStepsAmount;
-            if (outputsRightAmountOfSteps) {
-                rightStep = currentStep;
-                const output = { maxChartPoint: Math.ceil(realMax / rightStep) * rightStep, step: rightStep };
-                return output;
-            }
-        }
-        currentPower++;
-    }
-    return { maxChartPoint: 0, step: 0 };
+export default (
+	realMax: number,
+	maxStepsAmount: number = 7
+): { maxChartPoint: number; step: number } => {
+	let currentPower = -1;
+	let rightStep;
+
+	if (realMax < 5) {
+		return {
+			maxChartPoint: 5,
+			step: 1
+		};
+	}
+	while (!rightStep) {
+		let beautifulNumbers = [
+			10 * 10 ** currentPower,
+			20 * 10 ** currentPower,
+			25 * 10 ** currentPower,
+			50 * 10 ** currentPower
+		];
+		if (currentPower === -1) {
+			beautifulNumbers = [1, 2, 5];
+		}
+		for (const currentStep of beautifulNumbers) {
+			const outputsRightAmountOfSteps = Math.ceil(realMax / currentStep) <= maxStepsAmount;
+			if (outputsRightAmountOfSteps) {
+				rightStep = currentStep;
+				const output = {
+					maxChartPoint: Math.ceil(realMax / rightStep) * rightStep,
+					step: rightStep
+				};
+				return output;
+			}
+		}
+		currentPower++;
+	}
+	return { maxChartPoint: 0, step: 0 };
 };
