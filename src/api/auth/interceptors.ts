6b18492b5bb494bdc56b24bb2d5a1439954a5248
Mariia <mariia.kondak@nure.ua>
2024-11-25 16:38:19 +0100

    Added axios request interceptor

--- src/api/auth/interceptors.ts
+++ src/api/auth/interceptors.ts
@@ -1,6 +1,19 @@
 import { refreshAccessToken } from '.';
 import axiosCfg from '../config';
 
+axiosCfg.interceptors.request.use(
+	(config) => {
+	  const accessToken = localStorage.getItem('accessToken');
+	  if (accessToken) {
+		config.headers['Authorization'] = `Bearer ${accessToken}`;
+	  }
+	  return config;
+	},
+	(error) => {
+	  return Promise.reject(error);
+	}
+  );
+
 axiosCfg.interceptors.response.use(
 	(response) => response,
 	async (error) => {
